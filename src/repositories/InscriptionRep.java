d712f40cab4f5f584976687c7f505a1c7a32abb3
Matias Tomas <tomasrimolimatias@gmail.com>
2021-09-30 22:45:17 -0300

    Ej 3

--- src/repositories/InscriptionRep.java
+++ src/repositories/InscriptionRep.java
@@ -1,6 +1,7 @@
 package repositories;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -64,10 +65,25 @@ public class InscriptionRep {
 	@SuppressWarnings("unchecked")
 	public List<CareerReportDTO> getCareerOrderByNameAndYear(){
 		List<CareerReportDTO> result = new ArrayList<CareerReportDTO>();
-		Query query = em.createQuery(
+		/*Query query = em.createQuery(
 				"SELECT new dtos.CareerReportDTO(c.nameCareer, YEAR(i.graduation) as graduationYear, COUNT(i.graduation) as Egresados) "
 						+ "	FROM Career c, Inscription i WHERE c.id = i.career AND i.graduation != NULL GROUP BY c.nameCareer, graduationYear ORDER BY c.nameCareer ASC, graduationYear");
-	 	result = query.getResultList();
+	 	*/
+		Query query = em.createNativeQuery("select c.id_career, c.nameCareer, egresados.year, egresados.contadorEgresados, inscriptos.contadorInscipcion from career c join "
+				+ "(select id_career, YEAR(graduation) as year, COUNT(graduation) as contadorEgresados from inscription where graduation is not null GROUP by id_career, year)"
+				+ " as egresados on egresados.id_career = c.id_career join (select id_career, YEAR(start_date) as year, COUNT(start_date) as contadorInscipcion from inscription GROUP by id_career, year)"
+				+ " as inscriptos where inscriptos.id_career = c.id_career GROUP BY c.id_career, year order by c.nameCareer, year");
+		result = query.getResultList();
+		List<CareerReportDTO> aux = new ArrayList<CareerReportDTO>();
+		//(String nameCareer, int year, Long qEnrolled, Long qGraduates)
+		/*
+		for (CareerReportDTO r : result) {
+			System.out.println();
+			aux.add( new CareerReportDTO(r.getNameCareer(),r.getYear(),r.getqEnrolled(),r.getqGraduates()));
+			System.out.println("nombre dasd : "+ r.getNameCareer());
+		}
+		*/
+		//new CareerReportDTO(r.getNameCareer(),r.getYear(),r.getqEnrolled(),r.getqGraduates()))
 	 	return result;
 	}
 
